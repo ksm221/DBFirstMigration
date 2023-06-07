f893b538970f867b248c6ead3384aceb408b29e8
Karl <karl.mellikov@gmail.com>
2023-06-07 21:47:49 +0300

    add all

---
+++ DatabaseFirstMigration/DatabaseFirstMigration/Controllers/DImProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DatabaseFirstMigration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DImProductController : ControllerBase
+    {
+        private readonly AdventureWorksDw2019Context _context;
+        public DImProductController(AdventureWorksDw2019Context context) { 
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<DimProduct>>> GetDimProducts()
+        {
+            return Ok(await _context.DimProducts
+                //.Include(p => p.DimProductSubcategory)
+                .ToListAsync());
+        }
+    }
+}
+ 
